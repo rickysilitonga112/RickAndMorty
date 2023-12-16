9312bdad4ae7f40c36ec803f12487290103e1601
rickysilitonga112 <rickysilitonga112@gmail.com>
2023-12-16 10:21:36 +0700

    feat: add CharacterListView and CharacterListViewViewModel files

---
+++ CharacterListView.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterListView.swift
+//  RickAndMorty
+//
+//  Created by Ricky Silitonga on 16/12/23.
+//
+
+import UIKit
+
+///Hancle the spinner, character list view etc
+final class CharacterListView: UIView {
+
+}
---
+++ CharacterListViewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterListViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Ricky Silitonga on 16/12/23.
+//
+
+import Foundation
+
+final class CharacterListViewViewModel {
+    
+    
+    
+    
+    
+}
