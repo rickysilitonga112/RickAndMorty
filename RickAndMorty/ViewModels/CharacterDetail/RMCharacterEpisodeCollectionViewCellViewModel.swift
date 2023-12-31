43cea7df7937837c06ad21fae9b3944eab555931
rickysilitonga112 <rickysilitonga112@gmail.com>
2023-12-31 16:46:49 +0700

    feat: configure collection info and photo on character detail view

--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -6,7 +6,67 @@
 //
 
 import Foundation
+import UIKit
+
+protocol RMEpisodeDataRender {
+    var name: String { get }
+    var air_date: String { get }
+    var episode: String { get }
+}
 
 final class RMCharacterEpisodeCollectionViewCellViewModel {
+    private let episodeDataUrl: URL?
+    public let borderColor: UIColor
     
+    private var isFetching = false
+    private var dataBlock: ((RMEpisodeDataRender) -> Void)?
+    private var episode: RMEpisode? {
+        didSet {
+            guard let model = episode else {
+                return
+            }
+            dataBlock?(model)
+        }
+    }
+    
+    // MARK: - init
+    init(episodeDataUrl: URL?, borderColor: UIColor = .systemBlue) {
+        self.episodeDataUrl = episodeDataUrl
+        self.borderColor = borderColor
+    }
+    
+    // MARK: - public
+    public func registerForData(_ block: @escaping(RMEpisodeDataRender) -> Void) {
+        self.dataBlock = block
+    }
+    
+    public func fetchEpisode() {
+        guard !isFetching else {
+            if let model = episode {
+                dataBlock?(model)
+            }
+            return
+        }
+        
+        guard let url =  episodeDataUrl,
+              let request = RMRequest(url: url) else  {
+            return
+        }
+        
+        isFetching = true
+        
+        RMService.shared.execute(
+            request,
+            expecting: RMEpisode.self) { [weak self] result in
+                switch result {
+                case .success(let model):
+                    DispatchQueue.main.async {
+                        self?.episode = model
+                    }
+                case .failure(let error):
+                    print(String(describing: error))
+                }
+            }
+        
+    }
 }
