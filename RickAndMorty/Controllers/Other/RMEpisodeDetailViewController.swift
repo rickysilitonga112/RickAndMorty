4d2efdca0dc7bad6f67cc6f485476d225a6c1b8b
rickysilitonga112 <rickysilitonga112@gmail.com>
2024-01-24 11:16:44 +0700

    Removed file that shouldn't be tracked

--- RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -2,29 +2,28 @@
 //  RMEpisodeDetailViewController.swift
 //  RickAndMorty
 //
-//  Created by Ricky Silitonga on 31/12/23.
+//  Created by Ricky Silitonga on 01/02/24.
 //
 
 import UIKit
 
-final class RMEpisodeDetailViewController: UIViewController {
-    private let url: URL?
-    
-    init(url: URL?) {
-        self.url = url
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+class RMEpisodeDetailViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        
-        if let url = url {
-            print("URL: \(url)")
-        }
+
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
